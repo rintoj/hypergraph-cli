abcc3f321418dd415eeb9fadd384455d9f1ed4ec
Rinto Jose <rintoj@gmail.com>
2024-01-03 11:38:59 +0530

    feat: add --open to checkout

--- src/commands/checkout/checkout-command.ts
+++ src/commands/checkout/checkout-command.ts
@@ -1,18 +1,19 @@
 import { command, input } from 'clifer'
 import { ensureDir } from 'fs-extra'
 import { toDashedName } from 'name-util'
+import { readCache, saveCache } from '../../cache/cache'
 import { ProjectContext } from '../../environment/read-environment'
 import { resolveProject } from '../../project/project-service'
 import { withErrorHandler } from '../../util/error-handler'
 import { getProjectRoot } from '../../util/get-project-root'
 import { runCommand } from '../../util/run-command'
 import { writeSourceFiles } from '../../util/source-file-util'
-import { readCache, saveCache } from '../../cache/cache'
 import { ProjectType, useCheckoutQuery } from './use-checkout-query.gql'
 
 interface Props {
   projectId?: string
   skipCache?: boolean
+  open?: boolean
 }
 
 async function createContext(project: ProjectType): Promise<ProjectContext> {
@@ -31,7 +32,7 @@ async function ensureProjectDir({ projectRoot }: Pick<ProjectContext, 'projectRo
   await runCommand('git init', { cwd: projectRoot, silent: true })
 }
 
-export async function runCheckout({ skipCache, ...props }: Props) {
+export async function runCheckout({ open, skipCache, ...props }: Props) {
   return withErrorHandler(async () => {
     const selectedProject = await resolveProject(props)
     const context = await createContext(selectedProject as any)
@@ -41,6 +42,9 @@ export async function runCheckout({ skipCache, ...props }: Props) {
     await ensureProjectDir(context)
     await writeSourceFiles(context.projectRoot, project.sourceFiles)
     saveCache(context.projectRoot, { checkoutToken: checkoutResponse.data.checkout.next })
+    if (open) {
+      runCommand(`open ${context.projectRoot}/${toDashedName(project.name ?? '')}.code-workspace`)
+    }
   })
 }
 
@@ -48,4 +52,5 @@ export default command<Props>('checkout')
   .description('Checkout a project')
   .option(input('projectId').description('Specify the ID of the project to checkout').string())
   .option(input('skipCache').description('Disable cache (enabled by default)'))
+  .option(input('open').description('Open the project once it has been checked out'))
   .handle(runCheckout)
